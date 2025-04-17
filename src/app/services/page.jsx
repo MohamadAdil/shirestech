8f97cd1244982c5a02da805b74a7245557a91501
Mohamad Adil <adilansari8727@gmail.com>
2025-04-17 12:06:07 +0530

    '17-04-2025'

---
+++ src/app/services/page.jsx
@@ -0,0 +1,22 @@
+import InnerHero from "../components/common/sections/InnerHero";
+import GrowTogether from "../components/home/GrowTogether";
+import OurTestimonials from "../components/home/OurTestimonials";
+import WhyUs from "../components/home/WhyUs";
+import SectionSc from "../components/services/SectionSc";
+
+export default function Services() {
+  return (
+    <div>
+      <InnerHero
+        title="Digital Marketing"
+        description="We revolutionizing digital engagement with the world's first hyperrealistic, interactive AI platforms. From livestreaming to customer service, we're redefining human-AI interaction across industries."
+        imageSrc="/images/services-img/hero-img.png"
+        imageAlt="AI Technology"
+      />
+      <SectionSc/>
+      <WhyUs />
+      <OurTestimonials />
+      <GrowTogether />
+    </div>
+  );
+}
